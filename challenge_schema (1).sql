6efd4dca8084c87303da517643ca4d06a9335558
Nishat Sultana <nishatdu@yahoo.com>
2022-04-16 11:57:51 -0400

    adding files

--- challenge_schema (1).sql
+++ challenge_schema (1).sql
@@ -27,3 +27,8 @@ CREATE TABLE vine_table (
   vine TEXT,
   verified_purchase TEXT
 );
+
+SELECT * FROM review_id_table;
+SELECT * FROM products_table;
+SELECT * FROM customers_table;
+SELECT * FROM vine_table;
